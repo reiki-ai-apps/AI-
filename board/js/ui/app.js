0964428f00fb845e88b0037d15bbf5d9ee979172
hinata2462-eng <hinata246246@gmail.com>
2026-08-16 16:21:50 +0900

    Bust status module cache

--- board/js/ui/app.js
+++ board/js/ui/app.js
@@ -18,7 +18,7 @@ import { renderCalendarScreen } from './calendarView.js';
 import { renderApprovalsScreen } from './approvalsView.js';
 import { renderHistoryScreen } from './historyView.js';
 import { renderConnectionsScreen } from './connectionsView.js';
-import { renderStatusScreen } from './statusView.js';
+import { renderStatusScreen } from './statusView.js?v=2';
 import { hasIntentLink, consumeIntentLink } from './intentLink.js';
 
 const SCREENS = [
